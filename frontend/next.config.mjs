95076bcbd2ffd6012d9010ad39438f8b89552892
kvnwdev <iamkevinn@protonmail.com>
2024-10-03 22:36:31 -0400

    fix: docker tweaks

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -25,6 +25,23 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/components": "/app/components",
+      "@/constants": "/app/constants",
+      "@/hooks": "/app/hooks",
+      "@/hoc": "/app/hoc",
+      "@/models": "/app/models",
+      "@/pages": "/app/pages",
+      "@/utils": "/app/utils",
+      "@/app": "/app",
+      "@/store": "/app/store",
+      "@/types": "/app/types",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
